6fb225292526cf64a58a99fdaa1b551ee3d0488b
Denis <denis-boz@hotmail.com>
2023-06-12 20:43:34 +0300

    Решение задачи 36

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
+/*
 int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] resultArray = new int[size];
@@ -24,3 +25,34 @@ for (int i = 0; i < array.Length; i++)
     }
 }
 Console.WriteLine($"Количество четных чисел в массиве: {quantity}");
+*/
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] GetArray(int size)
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = new Random().Next(100);
+    }
+    return resultArray;
+}
+int[] array = GetArray(10);
+Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях равна {sum}]");
